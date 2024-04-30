d34e77be8add4721c2a75f093a9ddd687add1f0b
ishcenko <a_ishcenko@i.ua>
2024-04-30 13:21:47 +0300

    createSlice, lesson2, 1.01minute

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { postDetailsReducer } from './postDetailsReducer';
+
+export const store = configureStore({
+  reducer: {
+    postDetails: postDetailsReducer,
+  },
+});
